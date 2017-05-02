e44827084380c939b6dadadcb30c67cdc382d580
Felix <felix@1201.us>
2017-05-02 17:34:19 +0800

    ignore

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2017/5/2
+ * Time: 17:32
+ */
+
+
+return [
+    'hello' => 'world',
+];
---
+++ src/Ftest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2017/5/2
+ * Time: 17:33
+ */
+
+namespace Fuzuchang\Sms;
+
+
+class Ftest
+{
+    public function hello()
+    {
+        return 'hello';
+    }
+}
